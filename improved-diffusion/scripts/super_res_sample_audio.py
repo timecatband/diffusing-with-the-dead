7ed16bda109db9f48481c9a2922e11f46dae04a8
Robert Carr <racarrg@gmail.com>
2022-06-07 23:32:28 -0700

    lolcats

--- improved-diffusion/scripts/super_res_sample_audio.py
+++ improved-diffusion/scripts/super_res_sample_audio.py
@@ -50,8 +50,8 @@ def main():
             clip_denoised=args.clip_denoised,
             model_kwargs=model_kwargs,
         )
-        sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
-        sample = sample.permute(0, 2, 3, 1)
+#        sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
+ #       sample = sample.permute(0, 2, 3, 1)
         sample = sample.contiguous()
 
         all_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
