6fda95c5b5d767dc0401f56afc98610be81d1221
Robert Carr <racarrg@gmail.com>
2022-06-08 12:58:21 -0700

    lol

--- improved-diffusion/train-audio.sh
+++ improved-diffusion/train-audio.sh
@@ -1,4 +1,4 @@
-export MODEL_FLAGS="--image_size 32768 --num_channels 128 --num_res_blocks 3"
+export MODEL_FLAGS="--image_size 16384 --num_channels 128 --num_res_blocks 3"
 export DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
 export TRAIN_FLAGS="--lr 1e-4 --batch_size 1"
 python3 scripts/train_audio.py --data_dir ../data/wav/chunks $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
