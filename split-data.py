b7fb75eed8a7761e46ff69aed00e760ccd56e93e
Robert Carr <racarrg@gmail.com>
2022-06-08 12:50:07 -0700

    Lol

--- split-data.py
+++ split-data.py
@@ -5,7 +5,7 @@ import os
 
 def split_file(name):
     myaudio = AudioSegment.from_file(name , "wav") 
-    chunk_length_ms = 2975.527809307605 # pydub calculates in millisec
+    chunk_length_ms = 1487.7639046538025 # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
 
     #Export all of the individual chunks as wav files
@@ -23,6 +23,6 @@ os.system("mkdir data/wav/chunks")
 os.system("mv data/wav/*chunk*wav data/wav/chunks")
 for track in os.listdir("data/wav/chunks"):
   path = "data/wav/chunks/" + track
-  if os.path.getsize(path) != 262488:
+  if os.path.getsize(path) < 132000:
       print("Deleting bad sized chunk" + str(os.path.getsize(path)))
       os.system("rm " + path)
