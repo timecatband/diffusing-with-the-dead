f4b6b9081671d7ba53e25c1b9036d3cf4eb1e461
Robert Carr <racarrg@gmail.com>
2022-06-08 17:35:55 -0700

    lol

--- split-data.py
+++ split-data.py
@@ -27,6 +27,6 @@ os.system("mkdir data/wav/chunks")
 os.system("mv data/wav/*chunk*wav data/wav/chunks")
 for track in os.listdir("data/wav/chunks"):
   path = "data/wav/chunks/" + track
-  if os.path.getsize(path) < 8192*4:
+  if os.path.getsize(path) < 26504:
       print("Deleting bad sized chunk" + str(os.path.getsize(path)))
       os.system("rm " + path)
