019d7dbb76ac15ce59d8744089511eb13dbf2b64
Robert Carr <racarrg@gmail.com>
2022-06-09 12:20:56 -0700

    lol

--- train_audio.py
+++ train_audio.py
@@ -21,7 +21,7 @@ from mindiffusion.dataset import load_data
 
 
 def train_celeba(
-    n_epoch: int = 100, device: str = "cuda:1", load_pth: Optional[str] = None
+    n_epoch: int = 100, device: str = "cuda:0", load_pth: Optional[str] = None
 ) -> None:
 
     ddpm = DDPM(eps_model=NaiveUnet(2, 2, n_feat=128), betas=(1e-4, 0.02), n_T=1000)
