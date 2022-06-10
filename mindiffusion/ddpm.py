27973014ca8ad70562ca06b73e8633dceb597ad3
Robert Carr <racarrg@gmail.com>
2022-06-09 17:53:46 -0700

    lol

--- mindiffusion/ddpm.py
+++ mindiffusion/ddpm.py
@@ -57,7 +57,7 @@ class DDPM(nn.Module):
                 x_i, torch.tensor(i / self.n_T).to(device).repeat(n_sample, 1)
             )
             # racarr
-            x_i = x_i.clamp(0,1)
+ #           x_i = x_i.clamp(0,1)
    #         x_i = (
     #            self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
      #           + self.sqrt_beta_t[i] * z
